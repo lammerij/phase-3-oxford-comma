de16c07f23c61f94b0247b8fac8988a870f6b65d
+ <+>
2022-06-14 16:14:09 -0400

    commit tree 67f5d317eeef1ce84c73f6d460397ce563f35904

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        return array.join
+    elsif array.length == 2
+        return array.join(" and ")
+    else
+        array.length == 3
+        index = array[-1]
+        array[-1] = "and #{index}"
+        puts array
+        return array.join(", ")
+    end 
 end
